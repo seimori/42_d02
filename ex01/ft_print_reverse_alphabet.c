88a909366d56794e044927d9053758aaec341596
issei <issei.morimoto@gmail.com>
2018-07-04 14:14:15 +0200

    ex01

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imorimot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/04 14:05:05 by imorimot          #+#    #+#             */
+/*   Updated: 2018/07/04 14:11:45 by imorimot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int 	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char c = 'z';
+	while(c >= 'a')
+	{
+		ft_putchar(c);
+		c--;
+	}
+}
+
+int		main()
+{
+	ft_print_reverse_alphabet();
+	ft_putchar('\n');
+	return (0);
+}
