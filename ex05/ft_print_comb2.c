3304cbe405895bc82cacb30ae2e4a617fa4c9f79
issei <issei.morimoto@gmail.com>
2018-07-05 13:42:58 +0200

    ex05

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -6,11 +6,39 @@
 /*   By: imorimot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 23:07:08 by imorimot          #+#    #+#             */
-/*   Updated: 2018/07/05 10:34:56 by imorimot         ###   ########.fr       */
+/*   Updated: 2018/07/05 13:34:10 by imorimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_putchar(char c);
+
+void	ft_print_four(char i, char j, char k, char l)
+{
+	ft_putchar(i);
+	ft_putchar(j);
+	ft_putchar(' ');
+	ft_putchar(k);
+	ft_putchar(l);
+	if (i != '9' || j != '8')
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_second_number(char i, char j, char k, char l)
+{
+	while (k <= '9')
+	{
+		while (l <= '9')
+		{
+			ft_print_four(i, j, k, l);
+			l++;
+		}
+		l = '0';
+		k++;
+	}
+}
 
 void	ft_print_comb2(void)
 {
@@ -23,29 +51,11 @@ void	ft_print_comb2(void)
 	while (i <= '9')
 	{
 		j = '0';
-		while (j <= '8')
+		while (j <= '9')
 		{
 			k = i;
 			l = j + 1;
-			while (k <= '9')
-			{
-				while (l <= '9')
-				{
-					ft_putchar(i);
-					ft_putchar(j);
-					ft_putchar(' ');
-					ft_putchar(k);
-					ft_putchar(l);
-					if (i != '9' || j != '8')
-					{
-						ft_putchar(',');
-						ft_putchar(' ');
-					}
-					l++;
-				}
-				l = '0';
-				k++;
-			}
+			ft_second_number(i, j, k, l);
 			j++;
 		}
 		i++;
